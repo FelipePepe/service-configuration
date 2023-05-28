77137da75cf88503fa651cf9b47a39f2ca64bb0e
Felipe Pérez <felipepepe@gmail.com>
2023-05-28 13:56:10 +0200

    add new properties files

--- RunVault.bat
+++
@@ -1,2 +0,0 @@
-@echo off
-vault server --dev --dev-root-token-id="00000000-0000-0000-0000-000000000000"
--- almacenarProduct.bat
+++
@@ -1,5 +0,0 @@
-@echo off
-
-vault kv put secret/product @product.json 
-
-vault kv put secret/booking-microservice @booking-microservice.json 
